da5aac0fdc9fcf22247744cfec88a6a9a87f5bde
ewi josepha <ewijosepha@gmail.com>
2023-09-28 18:19:32 +0200

    fixing responsiveness

--- app.js
+++ app.js
@@ -1,11 +1,109 @@
-const rootContainer = document.getElementById('root-element')
-const root = ReactDOM.createRoot(rootContainer)
+const rootContainer = document.getElementById("root-element");
+const root = ReactDOM.createRoot(rootContainer);
 
-function App () {
-  const name = "hello"
-  return <div className="container">
-    <h1>Cash Register</h1>
-  </div>
+const money = [
+  {
+    id: 1,
+    name: "Cash $",
+    number: "number",
+  },
+  {
+    id: 2,
+    name: "Penny",
+    number: " ",
+  },
+  {
+    id: 3,
+    name: "Nickel",
+    number: "",
+  },
+  {
+    id: 4,
+    name: "Dime",
+    number: "",
+  },
+  {
+    id: 5,
+    name: "Quarter",
+    number: "",
+  },
+  {
+    id: 6,
+    name: "One",
+    number: "",
+  },
+  {
+    id: 7,
+    name: "Five",
+    number: "",
+  },
+  {
+    id: 8,
+    name: "Ten",
+    number: "",
+  },
+  {
+    id: 9,
+    name: "Twenty",
+    number: "",
+  },
+  {
+    id: 10,
+    name: "One Hundred",
+    number: "",
+  },
+];
+
+function App() {
+  let [bill, setBill] = React.useState(0);
+  let [bill2, setBill2] = React.useState(0);
+
+  function change(event) {
+    setBill(event.target.value);
+    console.log(bill);
+  }
+
+  function bil2(event) {
+    setBill2(event.target.value);
+    console.log(bill2);
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="wrapper">
+          <div className="firstDiv">
+            <h1>Bill Amount</h1>
+            <div className="input1">
+              <input
+                type="number"
+                value={bill}
+                onChange={(e) => change(e)}
+                className="btn1"
+              />
+              <input className="submit1" type="submit" placeholder="Submit" />
+            </div>
+            <h1>Cash Given</h1>
+            <div className="input2">
+              <input onChange={bil2} type="number" className="btn2" />
+              <input className="submit2" type="submit" placeholder="Submit" />
+            </div>
+          </div>
+          <h2>Return Change</h2>
+          <div className="secondDiv">
+            {money.map((money) => (
+              <div  key={money.id} className="all">
+                <h4>{money.name}</h4>
+                <div className="number">
+                  <h5>{money.number}</h5>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-root.render(<App />)
+root.render(<App />);
